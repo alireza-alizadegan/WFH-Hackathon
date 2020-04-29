d60c15b5c9763e5f50ee15161881210fa62e7b38
Alireza Alizadegan <a.r.alizadegan@gmail.com>
2020-04-28 20:19:42 -0700

    changed interest to role in all files

--- content_filtering.py
+++ content_filtering.py
@@ -4,18 +4,18 @@ def content_filtering(user):
     import numpy as np
     import pandas as pd
     
-    # read user interest and project interest data entered by users
-    user_interest_df = pd.read_csv('user_interest_df.csv', index_col=0)
-    project_interest_df = pd.read_csv('project_interest_df.csv', index_col=0)
+    # read user role and project role data entered by users
+    user_role_df = pd.read_csv('user_role_df.csv', index_col=0)
+    project_role_df = pd.read_csv('project_role_df.csv', index_col=0)
     
-    # pivot tables to show ratings for each interest in columns suitable for similarity analysis
-    user_rating_df = user_interest_df.pivot(index='user_id', columns='interest_id', values='rating')
+    # pivot tables to show ratings for each role in columns suitable for similarity analysis
+    user_rating_df = user_role_df.pivot(index='user_id', columns='role_id', values='rating')
     user_rating_df.fillna(0.0, inplace=True)
-    project_rating_df = project_interest_df.pivot(index='project_id', columns='interest_id', values='rating')
+    project_rating_df = project_role_df.pivot(index='project_id', columns='role_id', values='rating')
     project_rating_df.fillna(0.0, inplace=True)
     
-    # set of interests for each project
-    project_interest_series = project_interest_df.groupby('project_id')['interest_id'].agg(lambda x: set(x))
+    # set of roles for each project
+    project_role_series = project_role_df.groupby('project_id')['role_id'].agg(lambda x: set(x))
 
     # libraries for sparse matrix and cosine similarity 
     from scipy.sparse import csr_matrix
@@ -27,7 +27,7 @@ def content_filtering(user):
     # go through all projects in database
     for project in project_rating_df.index:
 
-        # calculate similarity of user interest with project interest
+        # calculate similarity of user role with project role
         user_rating_sparse = scipy.sparse.csr_matrix(user_rating_df.iloc[user, :].values)
         project_rating_sparse = scipy.sparse.csr_matrix(project_rating_df.iloc[project, :].values)
         similarity = cosine_similarity(user_rating_sparse, project_rating_sparse)
@@ -39,10 +39,10 @@ def content_filtering(user):
 
     # subset of similar projects compatible with user skill level 
     for project in recommended_projects:
-        for interest in project_interest_series.iloc[project]:
-            required_skills = (project_interest_df['project_id']==project) & (project_interest_df['interest_id']==interest)
-            qualified_skill = (user_interest_df['user_id']==user) & (user_interest_df['interest_id']==interest)
-            if project_interest_df[required_skills].experience.values > user_interest_df[qualified_skill].experience.values:
+        for role in project_role_series.iloc[project]:
+            required_skills = (project_role_df['project_id']==project) & (project_role_df['role_id']==role)
+            qualified_skill = (user_role_df['user_id']==user) & (user_role_df['role_id']==role)
+            if project_role_df[required_skills].experience.values > user_role_df[qualified_skill].experience.values:
                 recommended_projects.remove(project)
                 break
                 
--- match_filtering.py
+++ match_filtering.py
@@ -4,25 +4,25 @@ def match_filtering(user):
     import numpy as np
     import pandas as pd
 
-    # read user interest and project interest data entered by users
-    user_interest_df = pd.read_csv('user_interest_df.csv', index_col=0)
-    project_interest_df = pd.read_csv('project_interest_df.csv', index_col=0)
+    # read user role and project role data entered by users
+    user_role_df = pd.read_csv('user_role_df.csv', index_col=0)
+    project_role_df = pd.read_csv('project_role_df.csv', index_col=0)
     
-    # set of interests for each user and project
-    user_interest_series = user_interest_df.groupby('user_id')['interest_id'].agg(lambda x: set(x))
-    project_interest_series = project_interest_df.groupby('project_id')['interest_id'].agg(lambda x: set(x))
+    # set of roles for each user and project
+    user_role_series = user_role_df.groupby('user_id')['role_id'].agg(lambda x: set(x))
+    project_role_series = project_role_df.groupby('project_id')['role_id'].agg(lambda x: set(x))
     
-    # list of projects matching interests of inquiring user
-    is_interest_match = user_interest_series.iloc[user]==project_interest_series.values
-    interested_projects = project_interest_series[is_interest_match].index.tolist() 
+    # list of projects matching roles of inquiring user
+    is_role_match = user_role_series.iloc[user]==project_role_series.values
+    match_projects = project_role_series[is_role_match].index.tolist() 
     
-    # subset of interesting projects compatible with user skill level 
-    for project in interested_projects:
-        for interest in project_interest_series.iloc[project]:
-            required_skill = (project_interest_df['project_id']==project) & (project_interest_df['interest_id']==interest)
-            qualified_skill = (user_interest_df['user_id']==user) & (user_interest_df['interest_id']==interest)
-            if project_interest_df[required_skill].experience.values > user_interest_df[qualified_skill].experience.values:
-                interested_projects.remove(project)
+    # subset of match projects compatible with user skill level 
+    for project in match_projects:
+        for role in project_role_series.iloc[project]:
+            required_skill = (project_role_df['project_id']==project) & (project_role_df['role_id']==role)
+            qualified_skill = (user_role_df['user_id']==user) & (user_role_df['role_id']==role)
+            if project_role_df[required_skill].experience.values > user_role_df[qualified_skill].experience.values:
+                match_projects.remove(project)
                 break
-            
-    return interested_projects
+
+    return match_projects
