4ee5401e3890aec1def5a5fbfb79b3d1d58ca9b6
Alireza Alizadegan <a.r.alizadegan@gmail.com>
2020-04-28 17:50:01 -0700

    shortened code lines

--- match_filtering.py
+++ match_filtering.py
@@ -5,15 +5,13 @@ def match_filtering(user):
     project_interest_df = pd.read_csv('project_interest_df.csv', index_col=0)
     user_interest_series = user_interest_df.groupby('user_id')['interest_id'].agg(lambda x: set(x))
     project_interest_series = project_interest_df.groupby('project_id')['interest_id'].agg(lambda x: set(x))
-    interested_projects = project_interest_series[user_interest_series.iloc[user]==project_interest_series.values].index.tolist() 
+    is_interest_match = user_interest_series.iloc[user]==project_interest_series.values
+    interested_projects = project_interest_series[is_interest_match].index.tolist() 
     for project in interested_projects:
-    for interest in project_interest_series.iloc[project]:
-        if project_interest_df[(project_interest_df['project_id']==project) & (project_interest_df['interest_id']==interest)].experience.values > user_interest_df[(user_interest_df['user_id']==user) & (user_interest_df['interest_id']==interest)].experience.values:
-            interested_projects.remove(project)
-            break
+        for interest in project_interest_series.iloc[project]:
+            required_skill = (project_interest_df['project_id']==project) & (project_interest_df['interest_id']==interest)
+            qualified_skill = (user_interest_df['user_id']==user) & (user_interest_df['interest_id']==interest)
+            if project_interest_df[required_skill].experience.values > user_interest_df[qualified_skill].experience.values:
+                interested_projects.remove(project)
+                break
     return interested_projects
-
-
-
-
-
